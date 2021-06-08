a8a5ce8172d0b8ac6224ff2aef15d15d1dfc0ca8
leek2 <leek2@DESKTOP-SV5PDNI>
2021-06-07 19:31:41 -0500

    Add project files.

---
+++ environment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace environment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Jesus Martinez");
+        }
+    }
+}
